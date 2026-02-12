448867a9f59984e15d770b610de4707a96ce2ebf
farah-nissa <frhnissa@gmail.com>
2026-02-13 01:43:17 +0700

    Update about_project.py

--- about_project.py
+++ about_project.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def background():
-    st.title("E-Commerce App Insights")
+    st.title("E-Commerce App Analytics Project")
     st.markdown("""
     ## Project Overview
     Optimizing sales through user behavior analysis, this project delves into e-commerce app performance, uncovering insights to drive sales growth and predict purchase patterns based on in-app activity.    """)
